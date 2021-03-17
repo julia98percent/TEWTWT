320b287b803579cdab5bc3903b6024a6fdd54e31
julia98percent <ji_0315@naver.com>
2021-03-17 21:59:31 +0900

    add emoji library

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import Picker from "emoji-picker-react";
 
 function Modal() {
+  const App = () => {
+    const [chosenEmoji, setChosenEmoji] = useState(null);
+
+    const onEmojiClick = (event, emojiObject) => {
+      setChosenEmoji(emojiObject);
+    };
+
+    return (
+      <div>
+        {chosenEmoji ? (
+          <span id="content">{chosenEmoji.emoji}</span>
+        ) : (
+          <span></span>
+        )}
+        <Picker
+          disableSkinTonePicker="false"
+          onEmojiClick={onEmojiClick}
+          pickerStyle={
+            {
+              // zIndex: "221",
+              // margin: "120px",
+              // width: "50%",
+            }
+          }
+        />
+      </div>
+    );
+  };
+
   return (
     <>
-      <button className="dimmer">
+      <div className="dimmer">
         <div className="modalContainer" onClick={(e) => e.stopPropagation()}>
-          <div className="wrapper">냐~</div>
+          <div className="wrapper">
+            <App />
+          </div>
         </div>
-      </button>
+      </div>
     </>
   );
 }
